996ad14af44094e473d424e038a33e7dcb975b9f
Zerak Ayoub <Zerak.ayoub@gmail.com>
2026-03-10 06:03:00 -0700

    fixed the output format

--- DataLogger.cpp
+++ DataLogger.cpp
@@ -5,9 +5,9 @@ void DataLogger::log(const VitalSigns &v)
 {
     std::ofstream file("vitals_log.csv", std::ios::app);
 
-    file << v.timestamp << ","
-         << v.heartRate << ","
-         << v.spo2 << ","
-         << v.respirationRate
-         << "\n";
+    file << "\n"
+         << v.timestamp
+         << "Heart Rate: " << v.heartRate << "\n"
+         << "SpO2: " << v.spo2 << "\n"
+         << "Respiration Rate: " << v.respirationRate << "\n";
 }
